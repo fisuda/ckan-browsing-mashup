1e690ae5a9321d743d10358d1f2d6aa6ce1b88bf
Alejandro Rodriguez <alex.rodriguez@conwet.com>
2016-11-30 13:08:41 +0100

    update dependecies

--- macs/build.sh
+++ macs/build.sh
@@ -1,10 +1,10 @@
 cd google
-zip -r google.wgt config.xml
+zip -r google.wgt config.xml images
 mv google.wgt ..
 cd ../flotr2
-zip -r flotr2.wgt config.xml
+zip -r flotr2.wgt config.xml images
 mv flotr2.wgt ..
 cd ../highcharts
-zip -r highcharts.wgt config.xml
+zip -r highcharts.wgt config.xml images
 mv highcharts.wgt ..
 cd ..
